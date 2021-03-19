9a56faecd7b43c180ca1b53304d428e585770e6a
coherent17 <mnb51817@gmail.com>
2021-03-19 13:25:04 +0800

    test

--- scrapy_1.py
+++
@@ -1,9 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-tag = input("請輸入定位元素，class前面加上.，id前面加上# ")
-res = requests.get('http://pala.tw/class-id-example/')
-soup = BeautifulSoup(res.text, "lxml")
-
-for drink in soup.select('{}'.format(tag)):
-    print(drink.get_text())
